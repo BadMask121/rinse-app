3b6fcc7d39ed8d4c56ef698eb03daef033fd25e7
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2021-03-30 09:05:16 +0000

    documentation

---
+++ server/index.ts
@@ -0,0 +1,66 @@
+import express, { Request, Response } from "express";
+import expressJSDocSwagger from "express-jsdoc-swagger";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+const options = {
+  info: {
+    version: "1.0.0",
+    title: "Merchant Api",
+    license: {
+      name: "MIT",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/",
+      description: "The Stagin api url",
+    },
+  ],
+  security: {
+    BearerAuth: {
+      type: "http",
+      scheme: "bearer",
+    },
+    BasicAuth: {
+      type: "http",
+      scheme: "basic",
+    },
+  },
+  filesPattern: ["**/*.ts", "../src/**/*.ts"], // Glob pattern to find your jsdoc files (it supports arrays too ['./**/*.ts'])
+  baseDir: __dirname,
+  swaggerUIPath: "/doc", // SwaggerUI will be render in this url. Default: '/api-docs'
+  exposeSwaggerUI: true, // Expose OpenAPI UI. Default true
+  exposeApiDocs: false, // Expose Open API JSON Docs documentation in `apiDocsPath` path. Default false.
+  apiDocsPath: "/doc", // Open API JSON Docs endpoint. Default value '/v3/api-docs'.
+};
+
+(async () => {
+  try {
+    await app.prepare();
+    const server = express();
+
+    // Define variable
+    const listener = expressJSDocSwagger(server)(options as any);
+    listener.on("finish", (swaggerObject) => {
+      console.log(__dirname);
+      console.log(JSON.stringify(swaggerObject, null, 2), "open api"); // This will print JSON OpenAPI result
+    });
+
+    server.all("*", (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, (err?: any) => {
+      if (err) throw err;
+      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
--- src/pages/api/orders/[date].ts
+++ src/pages/api/orders/[date].ts
@@ -1,8 +1,41 @@
+/**
+ * Orders data type
+ * @typedef {object} CustomerOrders
+ *
+ * @property {string} first_name.required - The customer first name
+ * @property {string} last_name.required - The customer last name
+ * @property {string} lat.required - The customer location in latitude
+ * @property {string} lon.required - customer location in logitude
+ * @property {string} time_assigned.required - order assigning time
+ */
+
+/**
+ * Customers return data
+ * @typedef {object} successResponse
+ * @property {integer} code.required 200 - success code to return 200
+ * @property {CustomerOrders} data - data
+ */
+
+/**
+ * Customers order failed to return
+ * @typedef {object} failedResponse
+ * @property {integer} code.required 400 - error code to return error http codes
+ * @property {string} message - success message
+ */
+
 import path from "path";
 import { formatDate, readCSV } from "utils/helpers";
 
 import { CustomerOrder } from "../../../@types/orders";
 
+/**
+ * GET /api/orders/:date
+ * @summary This returns list of orders and its geometry, if given @param date will return orders on that date
+ * @param {string} date.query.required - specific date to get orders from
+ * @return {CustomerOrders} 200 - success response - application/json
+ * @return {failedResponse} 400 - Bad request response
+ * @tags Customer Orders
+ */
 export default async (req: any, res, next) => {
   try {
     const { date } = req.query;
